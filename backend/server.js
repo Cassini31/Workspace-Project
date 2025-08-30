587de777ef6ab49754cc9d1204580f030b205e27
Ey Alfaro <adrian.alfaro.fl2@gmail.com>
2025-08-30 20:13:46 +0800

    feat(backend): enable json request handling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
+
 const express = require('express');
 const app = express();
 const PORT = 4000;
+
+// Enable JSON request body parsing
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Backend is running with Express 🚀');
